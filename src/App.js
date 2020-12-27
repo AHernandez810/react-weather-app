f1cc56dc286058c721c539e41ac0555bf0dc72bf
AHernandez810 <ashleyhernandez810.ah@gmail.com>
2020-12-27 01:31:26 -0500

    Added axios intergration so it displayes the weather for a city

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+import React from "react";
+import Weather from "./Weather";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">  
+        <h1>
+         Hello World
+        </h1>
+        <Weather city="London" />
       </header>
     </div>
   );
---
+++ src/Weather.js
@@ -0,0 +1,23 @@
+import React from "react";
+import axios from "axios";
+import Loader from 'react-loader-spinner';
+
+export default function Weather(props) {
+    function handleResponse(response) {
+        alert(`The weather in ${response.data.name} is ${response.data.main.temp} C`)
+    }
+
+    let apiKey = "8542b50982e200d0127f4bfc3b2ce168"
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+    return (
+      <Loader
+         type="Puff"
+         color="#00BFFF"
+         height={100}
+         width={100}
+         timeout={3000} 
+      />
+    )
+}
